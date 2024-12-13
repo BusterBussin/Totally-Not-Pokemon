d55bc0b5deaeeba7da3fe2d3dbde894b46aa3a66
Buster <busterbussin@gmail.com>
2024-12-13 12:49:50 -0500

    saving added

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Main {
     public static void main(String args[]) {
@@ -19,6 +26,13 @@ public class Main {
         int catchrng;
         boolean shiny = false;
         ArrayList<String> arrLst = new ArrayList<String>();
+        // terrible attempt of doing a json, feel free to change
+        JSONObject pkmInfo = loadPKMInfoFromFile("pokemon.json");
+        if (pkmInfo == null) {
+            pkmInfo = new JSONObject();
+            pkmInfo.put("Pokemon", new JSONArray());
+        }
+        JSONArray pokemonList = (JSONArray) pkmInfo.get("Pokemon");
         System.out.println("Do you want to catch a Pokemon? (Y/N)");
         catching = scan.nextLine().toUpperCase().charAt(0);
         while (catching == 'Y' || catching == 'C') {
@@ -40,11 +54,16 @@ public class Main {
                         if (catchrng == 4) {
                             caught = true;
                             System.out.println(name + " was caught!");
+                            JSONObject newPokemon = new JSONObject();
                             if (shiny) {
-                                arrLst.add("Shiny " + name);
+                                // If shiny, save the pokemon as "Shiny" and the pokemon name
+                                newPokemon.put("Name", "Shiny" + name);
                             } else {
-                            arrLst.add(name);
+                                // Else, save the pokemon normally.
+                                newPokemon.put("Name", name);
                             }
+                            newPokemon.put("Shiny", shiny);
+                            pokemonList.add(newPokemon);
                         } else {
                             System.out.println(name + " broke free!");
                         }
@@ -70,14 +89,16 @@ public class Main {
             System.out.println("Would you like to catch another pokemon? (Y/N)\nYou can press I to view your pokemon.");
             catching = scan.nextLine().toUpperCase().charAt(0);
             if (catching == 'I') {
-                for (int i = 0; i < arrLst.size(); i++) {
-                    System.out.println(arrLst.get(i));
+                for (Object obj : pokemonList) {
+                    JSONObject pokemon = (JSONObject) obj;
+                    System.out.println("Name: " + pokemon.get("Name") + "\n Shiny: " + pokemon.get("Shiny"));
                 }
                 System.out.println("Press enter to continue.");
                 catching = 'Y';
                 scan.nextLine();
             }
         }
+        savePKMInfoToFile(pkmInfo, "pokemon.json");
         scan.close();
         System.exit(0);
     }
@@ -92,6 +113,29 @@ public class Main {
         } catch (Exception ex) {
             System.out.println("Error with playing sound.");
             ex.printStackTrace();
+        }
+    }
+
+    // Should load pokemon. feel free to change.
+    private static JSONObject loadPKMInfoFromFile(String filename) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(filename));
+            JSONObject jsonObject = (JSONObject) obj;
+            return jsonObject;
+        } catch (IOException | ParseException e) {
+            System.out.println("Error reading file, initializing empty album info.");
+            return null; // Return null if the file does not exist or there is an error
+        }
+    }
+
+    // Should save pokemon.
+    private static void savePKMInfoToFile(JSONObject pkmInfo, String filename) {
+        try (FileWriter file = new FileWriter(filename)) {
+            file.write(pkmInfo.toJSONString()); // Put the "Albums" key with the album data
+            file.flush();
+        } catch (IOException e) {
+            System.out.println("Error saving to file.");
         }
     }
 }
